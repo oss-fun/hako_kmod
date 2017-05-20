cab4a58439da73c8ffe75d12ffe61104b6b092d3
Katsuya Matsubara <matsu@fun.ac.jp>
2017-05-21 08:38:55 +0900

    Add empty functions for device file I/O

--- hello_kmod.c
+++ hello_kmod.c
@@ -1,15 +1,81 @@
 #include <linux/init.h>
 #include <linux/module.h>
+#include <linux/fs.h>
+#include <linux/cdev.h>
+#include <linux/uaccess.h>
+
+static int chardev_open(struct inode *inode, struct file *filp)
+{
+	printk(KERN_INFO "device is open.\n");
+	return 0;
+}
+
+static int chardev_release(struct inode *inode, struct file *filp)
+{
+	printk(KERN_INFO "device released.\n");
+	return 0;
+}
+
+static ssize_t chardev_read(struct file *filp, char __user *buff,
+			size_t count, loff_t *offp)
+{
+	printk(KERN_INFO "read from device.\n");
+	return count;
+}
+
+static ssize_t chardev_write(struct file *filp, const char __user *buff,
+			size_t count, loff_t *offp)
+{
+	printk(KERN_INFO "write to device.\n");
+	return count;
+}
+
+static struct file_operations chardev_fops = {
+	.owner = THIS_MODULE,
+	.open = chardev_open,
+	.read = chardev_read,
+	.write = chardev_write,
+	.release = chardev_release,
+};
+
+static struct cdev *cdev;
+static const dev_t dev = MKDEV(240, 0);
 
 static int __init hello_init(void)
 {
-	printk(KERN_INFO "Hello World\n");
-	return 0;
+	int err;
+
+	err = register_chrdev_region(dev, 1, "hello");
+	if (err) {
+		printk(KERN_ERR "register_chrdev_region failed.\n");
+		return -1;
+	}
+
+	cdev = cdev_alloc();
+	if (!cdev) {
+		printk(KERN_ERR "cdev_alloc failed.\n");
+		return -1;
+	}
+
+	cdev->ops = &chardev_fops;
+	err = cdev_add(cdev, dev, 1);
+	if (err) {
+		printk(KERN_ERR "cdev_add failed.\n");
+		return -1;
+	}
+
+	printk(KERN_INFO "hello device is ready for I/O.\n");
+
+	return err;
 }
 
 static void __exit hello_exit(void)
 {
-	printk(KERN_INFO "Bye World\n");
+	if (cdev)
+		cdev_del(cdev);
+	unregister_chrdev_region(dev, 1);
+
+	printk(KERN_INFO "hello device removed.\n");
 	return;
 }
 
