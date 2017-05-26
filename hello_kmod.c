8b161b00253e10e94930d9cb50eac8681d10d621
Katsuya Matsubara <matsu@fun.ac.jp>
2017-05-26 11:41:06 +0900

    Say hello! with the string which got through write()

--- hello_kmod.c
+++ hello_kmod.c
@@ -4,8 +4,9 @@
 #include <linux/cdev.h>
 #include <linux/uaccess.h>
 
-#define CHARDEV_BUFFLEN		1024
-static unsigned char chardev_buff[CHARDEV_BUFFLEN];
+#define CHARDEV_BUFFLEN		(1024 - 7)
+static unsigned char chardev_buff[CHARDEV_BUFFLEN + 7] = "Hello, ";
+static unsigned char chardev_len = 7;
 
 static int chardev_open(struct inode *inode, struct file *filp)
 {
@@ -22,8 +23,18 @@ static int chardev_release(struct inode *inode, struct file *filp)
 static ssize_t chardev_read(struct file *filp, char __user *buff,
 			size_t count, loff_t *offp)
 {
+	unsigned long remain;
+
 	printk(KERN_INFO "read from device.\n");
-	return count;
+	if (count <= chardev_len)
+		return -EINVAL;
+
+	remain = copy_to_user(buff, chardev_buff, chardev_len + 1);
+	if (remain > 0)
+		printk(KERN_INFO "%ld bytes remain in kernel buffer.\n",
+		       remain);
+
+	return chardev_len + 1 - remain;
 }
 
 static ssize_t chardev_write(struct file *filp, const char __user *buff,
@@ -32,15 +43,18 @@ static ssize_t chardev_write(struct file *filp, const char __user *buff,
 	unsigned long remain;
 
 	printk(KERN_INFO "write to device.\n");
-	if (count > CHARDEV_BUFFLEN)
+	if (count > CHARDEV_BUFFLEN + 2)
 		return -EINVAL;
 
-	remain = copy_from_user(chardev_buff, buff, count);
+	chardev_len = 7;
+	remain = copy_from_user(&chardev_buff[chardev_len], buff, count);
 	if (remain > 0)
 		printk(KERN_INFO "%ld bytes remain in user buffer.\n",
 		       remain);
-	chardev_buff[count - remain] = '\0';
-	printk(KERN_INFO "received string = %s.\n", chardev_buff);
+	chardev_len += count - remain;
+	chardev_buff[chardev_len] = '!';
+	chardev_buff[chardev_len + 1] = '\0';
+	printk(KERN_INFO "received string = %s.\n", &chardev_buff[7]);
 
 	return count - remain;
 }
