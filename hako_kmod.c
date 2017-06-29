e5887444d16893ecfe1f319788bc49de4b91217c
Katsuya Matsubara <matsu@fun.ac.jp>
2017-06-30 03:30:59 +0900

    Split device file interface for update each character of the 'hakodate' string

--- hako_kmod.c
+++ hako_kmod.c
@@ -23,13 +23,19 @@ static unsigned char chardev_buff[CHARDEV_BUFFLEN] = {
 
 static int chardev_open(struct inode *inode, struct file *filp)
 {
-	printk(KERN_INFO "hako: device open.\n");
+	int minor = iminor(inode);
+
+	printk(KERN_INFO "hako: device hako%d open.\n", minor);
+	filp->private_data = (void *)minor;
+
 	return 0;
 }
 
 static int chardev_release(struct inode *inode, struct file *filp)
 {
-	printk(KERN_INFO "hako: device released.\n");
+	int minor = iminor(inode);
+
+	printk(KERN_INFO "hako: device hako%d released.\n", minor);
 	return 0;
 }
 
@@ -37,34 +43,63 @@ static ssize_t chardev_read(struct file *filp, char __user *buff,
 			size_t count, loff_t *offp)
 {
 	unsigned long remain;
+	int minor = (int)filp->private_data;
 
-	printk(KERN_INFO "hako: read from device.\n");
-	if (count < CHARDEV_BUFFLEN)
-		return -EINVAL;
+	printk(KERN_INFO "hako: read from device hako%d.\n", minor);
 
-	remain = copy_to_user(buff, chardev_buff, CHARDEV_BUFFLEN);
-	if (remain > 0)
-		printk(KERN_INFO "hako: %ld bytes remain in kernel buffer.\n",
-		       remain);
+	if (minor == 0) {
+		if (count < CHARDEV_BUFFLEN)
+			return -EINVAL;
+		remain = copy_to_user(buff, chardev_buff,
+				      CHARDEV_BUFFLEN);
+		if (remain > 0)
+			printk(KERN_INFO "hako: %ld bytes remain "
+			       "in kernel buffer.\n", remain);
 
-	return CHARDEV_BUFFLEN - remain;
+		return CHARDEV_BUFFLEN - remain;
+	} else if (minor <= CHARDEV_BUFFLEN) {
+		if (put_user(chardev_buff[minor - 1], buff)) {
+			printk(KERN_ERR "hako: put_user failed.\n");
+			return -EFAULT;
+		}
+
+		return 1;
+	}
+
+	printk(KERN_ERR "hako: invalid minor number %d.\n",
+	       minor);
+	return -EINVAL;
 }
 
 static ssize_t chardev_write(struct file *filp, const char __user *buff,
 			size_t count, loff_t *offp)
 {
 	unsigned long remain;
+	int minor = (int)filp->private_data;
 
-	printk(KERN_INFO "hako: write to device.\n");
-	if (count < CHARDEV_BUFFLEN)
-		return -EINVAL;
+	printk(KERN_INFO "hako: write to device hako%d.\n", minor);
+	if (minor == 0) {
+		if (count < CHARDEV_BUFFLEN)
+			return -EINVAL;
 
-	remain = copy_from_user(chardev_buff, buff, CHARDEV_BUFFLEN);
-	if (remain > 0)
-		printk(KERN_INFO "hako: %ld bytes remain in user buffer.\n",
-		       remain);
+		remain = copy_from_user(chardev_buff, buff, CHARDEV_BUFFLEN);
+		if (remain > 0)
+			printk(KERN_INFO "hako: %ld bytes remain in user buffer.\n",
+			       remain);
 
-	return CHARDEV_BUFFLEN - remain;
+		return CHARDEV_BUFFLEN - remain;
+	} else if (minor <= CHARDEV_BUFFLEN) {
+		if (get_user(chardev_buff[minor - 1], buff)) {
+			printk(KERN_ERR "hako: get_user failed.\n");
+			return -EFAULT;
+		}
+
+		return 1;
+	}
+
+	printk(KERN_ERR "hako: invalid minor number %d.\n",
+	       minor);
+	return -EINVAL;
 }
 
 static struct file_operations chardev_fops = {
@@ -75,44 +110,40 @@ static struct file_operations chardev_fops = {
 	.release = chardev_release,
 };
 
-static struct cdev *cdev;
+static struct cdev cdev;
 static const dev_t dev = MKDEV(240, 0);
 
 static int __init hello_init(void)
 {
 	int err;
 
-	err = register_chrdev_region(dev, 1, "hako");
+	err = register_chrdev_region(dev, CHARDEV_BUFFLEN + 1, "hako");
 	if (err) {
 		printk(KERN_ERR "hako: register_chrdev_region failed.\n");
 		return -1;
 	}
 
-	cdev = cdev_alloc();
-	if (!cdev) {
-		printk(KERN_ERR "hako: cdev_alloc failed.\n");
-		return -1;
-	}
-
-	cdev->ops = &chardev_fops;
-	err = cdev_add(cdev, dev, 1);
+	cdev_init(&cdev, &chardev_fops);
+	cdev.owner = THIS_MODULE;
+	err = cdev_add(&cdev, dev, CHARDEV_BUFFLEN + 1);
 	if (err) {
 		printk(KERN_ERR "hako: cdev_add failed.\n");
 		return -1;
 	}
 
-	printk(KERN_INFO "hako: device is ready for I/O.\n");
+	printk(KERN_INFO "hako: all %d devices are ready for I/O.\n",
+		CHARDEV_BUFFLEN + 1);
 
 	return err;
 }
 
 static void __exit hello_exit(void)
 {
-	if (cdev)
-		cdev_del(cdev);
-	unregister_chrdev_region(dev, 1);
+	cdev_del(&cdev);
+	unregister_chrdev_region(dev, CHARDEV_BUFFLEN + 1);
 
-	printk(KERN_INFO "hako: device removed.\n");
+	printk(KERN_INFO "hako: all %d devices removed.\n",
+		CHARDEV_BUFFLEN + 1);
 	return;
 }
 
--- hakocli/hakocli.c
+++ hakocli/hakocli.c
@@ -8,12 +8,14 @@
 #include <ctype.h>
 
 #define BUFLEN	(8)
+#define PATHLEN	(256)
 
 int main(int argc, char *argv[])
 {
 	int fd, it;
 	unsigned char buf[BUFLEN];
-	int exclusive;
+	int exclusive, nth;
+	char devfile[PATHLEN];
 
 	if ((argc < 4) || (argc > 5)) {
 		fprintf(stderr, "Usage: %s file n_th n_iterate [-x]\n",
@@ -26,22 +28,23 @@ int main(int argc, char *argv[])
 	else
 		exclusive = 0;
 
-	fd = open(argv[1], O_RDWR);
+	nth = strtol(argv[2], NULL, 0);
+	snprintf(devfile, PATHLEN, "%s%d", argv[1], nth);
+	fd = open(devfile, O_RDWR);
 	if (fd < 0) {
-		perror("open");
+		perror(devfile);
 		return -1;
 	}
 
 	it = strtoul(argv[3], NULL, 0);
 	do {
-		unsigned char *pc;
-		unsigned int nth;
 		size_t len, l;
+		const size_t unit_len = (nth == 0) ? BUFLEN : 1;
 
 		if (exclusive)
 			flock(fd, LOCK_EX);
-		for (len = 0; len < BUFLEN; len += l) {
-			l = read(fd, &buf[len], BUFLEN - len);
+		for (len = 0; len < unit_len; len += l) {
+			l = read(fd, &buf[len], unit_len - len);
 			if (l < 0) {
 				perror("read");
 				if (exclusive)
@@ -52,26 +55,19 @@ int main(int argc, char *argv[])
 		}
 
 		if (it <= 0) {
-			buf[BUFLEN] = '\0';
+			buf[len] = '\0';
 			puts((const char *)buf);
 			if (exclusive)
 				flock(fd, LOCK_UN);
 			break;
 		}
 
-		nth = strtoul(argv[2], NULL, 0);
-		if (nth > BUFLEN) {
-			fprintf(stderr, "invalid n_th %d\n", nth);
-			if (exclusive)
-				flock(fd, LOCK_UN);
-			close(fd);
-			return -1;
-		}
-		pc = &buf[nth - 1];
-		*pc = islower(*pc) ? toupper(*pc) : tolower(*pc);
+		if (nth > 0)
+			buf[0] = islower(buf[0]) ?
+				toupper(buf[0]) : tolower(buf[0]);
 
-		for (len = 0; len < BUFLEN; len += l) {
-			l = write(fd, &buf[len], BUFLEN - len);
+		for (len = 0; len < unit_len; len += l) {
+			l = write(fd, &buf[len], unit_len - len);
 			if (l < 0) {
 				perror("write");
 				if (exclusive)
